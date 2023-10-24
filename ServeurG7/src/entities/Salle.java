abd5a4aff6c5df9b5ce64592f5522a868cf03d75
Oumaima EL MARDI <82413301+oumaimaelmardi@users.noreply.github.com>
2023-10-24 21:47:02 +0200

    gestion machine

---
+++ ServeurG7/src/entities/Salle.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+/**
+ *
+ * @author ATLAS PRO ELECTRO
+ */
+@Entity
+@NamedQuery(name = "findAllSalle", query = "from Salle")
+public class Salle implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+    
+    public Salle() {
+    }
+
+    public Salle(String code) {
+        this.code = code;
+    }
+
+    public Salle(int id, String code) {
+        this.id = id;
+        this.code = code;
+    }
+
+  
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+  
+    @Override
+    public String toString() {
+            return code;
+    }
+}
